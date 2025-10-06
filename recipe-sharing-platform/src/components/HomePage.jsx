b12f468ea5ed5d269983e7bde2b6df0e8df0d0a5
Michael Essam <145896866+Michael-Esam@users.noreply.github.com>
2025-10-06 15:09:43 +0300

    Update HomePage.jsx

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 
 const HomePage = ({ recipes }) => {
-  ["useState", "useEffect", "data.json"]
+  // ["useState", "useEffect", "data.json"]
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
       {recipes.map((recipe) => (
